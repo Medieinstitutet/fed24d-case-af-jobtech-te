45c32ba1941739c1a9bfe5f8aa22032d3e006fd3
thnielseen <tess.prog@gmail.com>
2025-09-12 10:47:57 +0200

    refactor(service): update jobService to use generic fetchData function and update comments

--- src/services/jobService.ts
+++ src/services/jobService.ts
@@ -1,39 +1,49 @@
+// /src/services/jobService.ts
 import { Job, type TypeaheadItem } from "../models/Job";
+import { fetchData } from './serviceBase';
 
 const baseUrl = "https://jobsearch.api.jobtechdev.se/";
 
+/**
+ * Get jobs from JobTech API
+ * - supports query, offset and limit
+ * - returns an array of Job objects
+ */
 export async function fetchJobs (query: string, offset: number, limit: number): Promise<Job[]> {
   const apiUrl = `${baseUrl}/search?q=${encodeURIComponent(query)}&offset=${offset}&limit=${limit}`;
+  const errorMsg = "Något gick fel vid hämtning av jobb";
 
-  const response = await fetch(apiUrl);
-  if (!response.ok) {
-    throw new Error("Något gick fel vid hämtning av jobb");
-  }
-  const data = await response.json();
+  const data = await fetchData<{ hits: Job[] }>(apiUrl, errorMsg);
+
   console.log("fetchJobs full API-response:", "query:", query, "offset:", offset, "limit:", limit, "data:", data);
-  return data.hits as Job[];
+  return data.hits;
 }
 
+
+/**
+ * Get a single job by its ID
+ * - returns a Job object
+ */
 export async function fetchJobById (id: string): Promise<Job> {
-  const apiUrl = `${baseUrl}/ad${id}`
+  const apiUrl = `${baseUrl}/ad/${id}`
+  const errorMsg = "Något gick fel vid hämtning av jobbdetaljer";
 
-  const response = await fetch(apiUrl);
-  if (!response.ok) {
-    throw new Error("Något gick fel vid hämtning av jobbdetaljer");
-  }
-  const data = await response.json();
+  const data = await fetchData<Job>(apiUrl, errorMsg);
+
   console.log("fetchJobById full API-response:", "id:", id, "data:", data);
-  return data as Job;
+  return data;
 }
 
+/**
+ * Get job suggestions (autocomplete)
+ * - returns an array of TypeaheadItem
+ */
 export async function fetchJobSuggestions (query: string): Promise<TypeaheadItem[]> {
   const apiUrl = `${baseUrl}/complete?q=${encodeURIComponent(query)}`;
+  const errorMsg = "Något gick fel vid hämtning av förslag";
+  const data = await fetchData<{ typeahead: TypeaheadItem[] }>(apiUrl, errorMsg);
 
-  const response = await fetch(apiUrl);
-  if (!response.ok) {
-    throw new Error("Något gick fel vid hämtning av förslag");
-  }
-  const data = await response.json();
-  /*   console.log("fetchJobSuggestion full API-response:", data); */
-  return data.typeahead || [];
+  console.log("fetchJobSuggestion full API-response:", data);
+
+  return data.typeahead ?? [];
 }
