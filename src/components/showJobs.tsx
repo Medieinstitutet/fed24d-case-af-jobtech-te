132fa71df28946ddc9cda0092cf1c7855f80b1c6
thnielseen <tess.prog@gmail.com>
2025-09-15 20:00:10 +0200

    feat(style): Implement base block to jobspage

--- src/components/showJobs.tsx
+++ src/components/showJobs.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router";
-import { JobItem, JobsListContainer, JobHeadline, EmployerName } from "./styled/JobsPage/ShowJobsComponents";
+import { JobItem, JobHeadline, EmployerName } from "./styled/JobsPage/ShowJobsComponents";
 import type { IJobAd } from '../models/IJobAd';
-import { MainWrap } from "./styled/LayoutWrappers";
+import { BaseBlockWrap } from './styled/Globals/Wrappers';
 interface ShowJobsProps {
   jobs: IJobAd[];
   adTotal: number;
@@ -9,21 +9,20 @@ interface ShowJobsProps {
 }
 
 export const ShowJobs = ({ jobs, adTotal }: ShowJobsProps) => {
- 
+
 
   return (
-    <JobsListContainer>
-      <h1>Lediga Jobb</h1>
-      <p>Det finns {adTotal} annonser som matchar din sökning</p>
-    {jobs.map(job => (
-      <JobItem key={job.id}>
-        <Link to={`/jobs/${job.id}`}>
-          <JobHeadline>{job.headline}</JobHeadline>
-        </Link>
-        <EmployerName>{job.employer?.name}</EmployerName>
-        <p>{job.occupation_field?.label}</p>
-      </JobItem>
-    ))}
-  </JobsListContainer>
-)
+    <BaseBlockWrap>
+      <p>Det finns <strong>{adTotal} </strong>annonser som matchar din sökning</p>
+      {jobs.map(job => (
+        <JobItem key={job.id}>
+          <Link to={`/jobs/${job.id}`}>
+            <JobHeadline>{job.headline}</JobHeadline>
+          </Link>
+          <EmployerName>{job.employer?.name}</EmployerName>
+          <p>{job.occupation_field?.label}</p>
+        </JobItem>
+      ))}
+    </BaseBlockWrap>
+  )
 };
